5f9173baeb8da0f0e34ef05f7fbd86495ca45481
BenjaminCoux <benjamin.coux@etu.univ-cotedazur.fr>
2022-02-19 13:51:45 +0100

    ajout page login

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,4 +1,4 @@
-import {StyleSheet, TouchableOpacity} from 'react-native';
+import {Modal, Picker, Pressable, StyleSheet, TextInput, TouchableOpacity} from 'react-native';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
@@ -6,17 +6,46 @@ import { RootTabScreenProps } from '../types';
 import {Ionicons} from "@expo/vector-icons";
 import {Image} from 'react-native' ;
 import {Camera} from "react-native-vision-camera";
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 
 export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
+    const [modal, setmodal] = useState(true);
+    const [filterValue,setfilterValue] = useState("");
+    const [selectedDropDown, setselectedDropDown] = useState("none");
     const getCameraPermission = async () => {
         await Camera.getCameraPermissionStatus()
     };
     useEffect(() => {
         getCameraPermission();
     }, []);
+
+    function modale(){
+        return(
+        <Modal visible={modal} style={{flex:1,justifyContent:"flex-end",alignItems:"center",margin:40}}>
+            <View style={{flex:1,justifyContent:"flex-end",alignItems:"center",margin:40}}>
+                <Text>Nom utilisateur : </Text>
+                <TextInput value={filterValue}  style={styles.input} onChangeText={setfilterValue}/>
+                <Text>Régime alimentaire</Text>
+                <Picker
+                    selectedValue={selectedDropDown}
+                    style={{ height: 50, width: 150 }}
+                    onValueChange={(itemValue, itemIndex) => setselectedDropDown(itemValue)}>
+                    <Picker.Item label="Aucun" value="none" />
+                    <Picker.Item label="Vegetarien" value="vege" />
+                </Picker>
+                <TouchableOpacity  onPress={()=>setmodal(false)} style={{flex: 1,
+                    alignItems: 'center',
+                    justifyContent: 'center'}}>
+                    <Text style={{borderRadius:5, backgroundColor:"#a4a4a4"}}>Page d'accueil</Text>
+                </TouchableOpacity>
+            </View>
+
+        </Modal>);
+    }
+
   return (
     <View style={styles.container}>
+        {modale()}
             <Text style={styles.title}>Accueil</Text>
             <Image source={require('../assets/images/eatsmart.png')} />
             <TouchableOpacity  style={styles.profile} onPress={()=>{
@@ -75,6 +104,13 @@ const styles = StyleSheet.create({
       backgroundColor:"white",
       borderRadius:100,
   },
+    input: {
+        height: 40,
+        margin: 12,
+        width:200,
+        borderWidth: 1,
+        padding: 10,
+    },
   title: {
       fontSize: 13,
       fontWeight: 'bold',
